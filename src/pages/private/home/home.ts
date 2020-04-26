7fbcf302810e36f1aca5eedf9795d8a7f3977237
Jenner <jenner.alagao@gmail.com>
2020-04-26 23:48:34 +0800

    pages adjustment

--- src/pages/private/home/home.ts
+++ src/pages/private/home/home.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 
-
-import { LogoutPage } from '../logout/logout';
-import { ChangePasswordPage } from '../change-password/change-password';
-import { UserProfilePage } from '../user-profile/user-profile';
-
-
 import { CommonServiceProvider } from '../../../providers/common-service/common-service';
 
 @Component({
@@ -20,24 +14,7 @@ export class HomePage {
   }
 
   initializePage() {
-  	return this.service.toastError('testing service error handler');
+  	// return this.service.toastError('testing service error handler');
   }
-
-  logout() {
-    this.navCtrl.setRoot(LogoutPage);
-  }
-
-  profile() {
-    this.navCtrl.setRoot(UserProfilePage);
-  }
-
-  changePassword() {
-    const modal = this.modalCtrl.create(ChangePasswordPage);
-    modal.present();
-  }
-
-
-  
-
 
 }

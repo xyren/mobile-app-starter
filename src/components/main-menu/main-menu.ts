2e8d94c0e6fac7e19f3c352ffb5dafb05e21cddd
Jenner <jenner.alagao@gmail.com>
2020-04-26 16:54:15 +0800

    compoment adjustment

--- src/components/main-menu/main-menu.ts
+++ src/components/main-menu/main-menu.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { HomePage } from '../../pages/private/home/home';
+// import { ViewPostPage } from '../../pages/common/view-post/view-post';
+import { UserProfilePage } from '../../pages/private/user-profile/user-profile';
+// import { NotificationPage } from '../../pages/common/notification/notification';
+// import { SettingsPage } from '../../pages/common/settings/settings';
+// import { OfficeMatesPage } from '../../pages/common/office-mates/office-mates';
+import { AdminMenuPage } from '../../pages/private/admin-menu/admin-menu';
+
 
 /**
  * Generated class for the MainMenuComponent component.
@@ -12,11 +22,42 @@ import { Component } from '@angular/core';
 })
 export class MainMenuComponent {
 
-  text: string;
+  shadowClass: string = 'text-shadow';
+	isAdmin: boolean = false;
+	pages: any = [];
+	@Input('active') active: string;
+  @Input('shadow') shadow: boolean = true;
+	@Input('menuColor') menuColor: string = 'text-black';
 
-  constructor() {
+  constructor(public navCtrl: NavController,) {
     console.log('Hello MainMenuComponent Component');
-    this.text = 'Hello World';
+
+    this.initializeMenu();
+  }
+
+	initializeMenu() {
+
+		this.pages = [
+      { title: 'Home', component: HomePage, icon: 'ios-home' },
+      { title: 'Profile', component: UserProfilePage, icon: 'ios-person'},
+      { title: 'Notifications', component: HomePage, icon: 'ios-notifications'},
+      { title: 'Settings', component: HomePage, icon: 'ios-settings'},
+      { title: 'Office Mates', component: HomePage, icon: 'ios-contacts'},
+      { title: 'Others', component: AdminMenuPage, icon: 'menu'},
+    ];    
+	}
+
+  ngAfterViewInit() {
+    if (this.shadow == false ) {
+      this.shadowClass = 'text-no-shadow';
+    }
+  }
+
+  viewPage(page) {
+    // console.log(this.menu , isSelf);
+    // this.navCtrl.setRoot(page.component, {}, {animate: true, animation: 'fadeInUp', duration: 1000});
+    this.navCtrl.setRoot(page.component, {}, {animate: true, direction: 'forward'});
+    // this.navCtrl.setRoot(page.component, {}, {animate: true, animation: 'ios-transition'});
   }
 
 }
